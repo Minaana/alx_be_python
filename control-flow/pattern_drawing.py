4dca8441922e2c5d387cc2ba1aea446b39b9df27
minaana <emmanuelnanaopokumanu@oulook.com>
2024-06-29 16:52:16 -0400

    created a Drawing Patterns with Nested Loops

--- control-flow/pattern_drawing.py
+++ control-flow/pattern_drawing.py
@@ -1,3 +1,7 @@
 size = int(input("Enter the size of the pattern: "))
-for _ in range(size):
-    print("*" * size)
+row = 0
+while row < size:
+    for _ in range(size):
+        print("*", end="")
+        print()
+        row += 1
