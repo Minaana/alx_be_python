48c5e6dd932e8f56e805e57aedcd76b1c4d34728
minaana <emmanuelnanaopokumanu@oulook.com>
2024-06-29 17:25:02 -0400

    created a Personal Daily Reminder

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -8,7 +8,7 @@ match priority :
         else :
             print(task, "is a High priority that requires immediate attention!: ",)
     case "low" :
-        if time == "yes":
+        if time_bound == "yes":
             print(task, "is a Low priority that can be done later today!: ",)
         else    :
             print(task, "is a low priority task. Consider completing it when you have free time.: ",)
