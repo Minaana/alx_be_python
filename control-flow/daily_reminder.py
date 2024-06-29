e98bb74c1bd21977844c8fe3e6dac5ccf597afa9
minaana <emmanuelnanaopokumanu@oulook.com>
2024-06-29 17:22:52 -0400

    created a Personal Daily Reminder

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -3,12 +3,12 @@ priority = input("Priority (high/medium/low): ")
 time = input("Is it time-bound? (yes/no):")
 match priority :
     case "high" :
-        if time == "yes" :
+        if time == "yes":
             print(task, "is a High priority that requires immediate attention today!: ",)
         else :
             print(task, "is a High priority that requires immediate attention!: ",)
     case "low" :
-        if time == "yes" :
+        if time == "yes":
             print(task, "is a Low priority that can be done later today!: ",)
         else    :
             print(task, "is a low priority task. Consider completing it when you have free time.: ",)
